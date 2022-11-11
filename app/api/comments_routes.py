94524819d41e556c545ce852fdb2b4a8327c65e2
Jon Park <jonpark13@gmail.com>
2022-11-11 18:53:28 -0500

    comment routes test

---
+++ app/api/comments_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Comment
+
+comments_routes = Blueprint('comments', __name__)
+
+
+@comments_routes.route('/')
+@login_required
+def comments():
+    """
+    Query for all users and returns them in a list of user dictionaries
+    """
+    all_coms = Comment.query.all()
+    return {'comments': [com.to_dict() for com in all_coms]}
