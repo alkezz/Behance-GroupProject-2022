c613f892900773e271c656fbcad8f8f8da7a72b3
Jon Park <jonpark13@gmail.com>
2022-11-11 18:57:06 -0500

    comment test

--- app/api/comments_routes.py
+++ app/api/comments_routes.py
@@ -9,7 +9,7 @@ comments_routes = Blueprint('comments', __name__)
 @login_required
 def comments():
     """
-    Query for all users and returns them in a list of user dictionaries
+    Query for all comments and returns them in a list of user dictionaries
     """
     all_coms = Comment.query.all()
     return {'comments': [com.to_dict() for com in all_coms]}
