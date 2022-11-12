3c72e65970279f4e498dd57dbe56570a1865db15
Ali <a.k.ezzeddine@gmail.com>
2022-11-12 13:05:50 -0500

    added project routes

---
+++ app/api/project_routes.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Project
+
+# Created our blueprint for our projects route, connecting it to our __init__.py
+project_routes = Blueprint("projects", __name__)
+
+@project_routes.route("/")
+def projects():
+    """
+    Query for all projects and returns them as a json dictionary
+    Url:
+    "/api/projects"
+    """
+    all_projects = Project.query.all()
+    return {'Projects': [project.to_dict() for project in all_projects]}
+
+@project_routes.route("/<int:id>/")
+def project_by_id(id):
+    """
+    Query for project by its id returns it as a json dictionary
+    Url:
+    "/api/projects/:id/"
+    """
+    one_project = Project.query.get(id)
+    if one_project:
+        return one_project.to_dict()
+    else:
+        return f"No such project with id of {id}"
