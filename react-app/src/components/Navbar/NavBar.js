291b90eb51c2434247b3800841787c18a232a92f
Christo Grabowski <christo.grab@gmail.com>
2022-11-13 15:50:57 -0600

    first pass at navbar conditional logic plus styling

---
+++ react-app/src/components/Navbar/NavBar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import LogoutButton from '../auth/LogoutButton';
+import LoginForm from '../auth/LoginForm'
+import SignUpForm from '../auth/SignUpForm'
+import './Navbar.css';
+
+const NavBar = () => {
+
+  const sessionUser = useSelector(state => state.session.user);
+
+  let sessionLinks;
+
+  if (sessionUser) {
+    sessionLinks = (
+      <div className="top-navbar-right">
+        <button id="new-project-button">Share Your Work</button>
+        <LogoutButton />
+      </div>
+    )
+  } else {
+    sessionLinks = (
+      <div className='top-navbar-right'>
+        <Link to='/login' exact={true} id="login-button">
+          Login
+        </Link>
+        <Link to='/sign-up' exact={true} id='signup-button'>
+          Sign Up
+        </Link>
+      </div>
+    )
+  }
+
+  return (
+    <nav id="top-navbar">
+      <div id="top-navbar-left">
+        <div id="enhance-logo-container">
+          <Link to='/' exact={true} id="enhance-logo">
+            Enhance
+          </Link>
+        </div>
+      </div>
+        {sessionLinks}
+    </nav>
+  );
+}
+
+export default NavBar;
