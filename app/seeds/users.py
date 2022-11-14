10702ae8ec93bf1edb72ecaada8085b4b51b8df0
jonpark13 <36808975+jonpark13@users.noreply.github.com>
2022-11-14 16:54:50 -0500

    Update users.py

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -52,7 +52,7 @@ def seed_users():
 def undo_users():
     if environment == "production":
         db.session.execute(f'DROP SCHEMA {SCHEMA} CASCADE;')
-        db.session.execute(f'CREATE SCHEMA {SCHEMA};'
+        db.session.execute(f'CREATE SCHEMA {SCHEMA};')
 #         db.session.execute(f'TRUNCATE table {SCHEMA}.projects RESTART IDENTITY CASCADE;')
 #         db.session.execute(f'TRUNCATE table {SCHEMA}."users" RESTART IDENTITY CASCADE;')
 #         db.session.execute(f"TRUNCATE table {SCHEMA}.projects RESTART IDENTITY CASCADE;")
