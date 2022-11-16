b7987773e4eca9e1f0ace317cb1b87ee22989c29
Christo Grabowski <christo.grab@gmail.com>
2022-11-16 15:28:26 -0600

    create project form linked to navbar button and basic styling

--- react-app/src/components/CreateProjectForm/index.js
+++ react-app/src/components/CreateProjectForm/index.js
@@ -14,7 +14,7 @@ function CreateProject() {
     const [is_preview, setIsPreview] = useState(true)
     const [errors, setErrors] = useState([])
     const [proj, setProj] = useState({});
-    
+
     if (!sessionUser) {
         return null
     }
@@ -45,56 +45,44 @@ function CreateProject() {
         dispatch(projectActions.createProject(new_project, project_images))
     }
     return (
-        <>
-            <div>
-                <form onSubmit={handleSubmit} action="/api/projects/project-images/upload" method="post" encType="multipart/form-data">
-                    <h1>Create a project!</h1>
-                    <label>
-                        <div>
-                            <input
-                                type='text'
-                                placeholder="Name of Project"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                className="create-project-fields"
-                            />
-                        </div>
-                        <div>
-                            {errors.map((error, idx) =>
-                                error === "Name but be between 10 and 50 characters" ? <li key={idx} id='error-list'>{error}</li> : null
-                            )}
-                        </div>
-                    </label>
-                    <label>
-                        <div>
-                            <input
-                                type='text'
-                                placeholder="Description of Project"
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                                className="create-project-fields"
-                            />
-                        </div>
-                        <div>
-                            {errors.map((error, idx) =>
-                                error === "Description must be between 20 and 50 characters" ? <li key={idx} id='error-list'>{error}</li> : null
-                            )}
-                        </div>
-                    </label>
+        <div className="create-project-container">
+            <form className="create-project-form" onSubmit={handleSubmit} action="/api/projects/project-images/upload" method="post" encType="multipart/form-data">
+                <h1>Start building your project:</h1>
+                <label>Project Name</label>
+                <input
+                    type='text'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="create-project-fields" />
+                <div>
+                    {errors.map((error, idx) =>
+                        error === "Name but be between 10 and 50 characters" ? <li key={idx} id='error-list'>{error}</li> : null
+                    )}
+                </div>
+                <label>Provide a brief description of your project</label>
+                    <input
+                        type='text'
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="create-project-fields" />
                     <div>
-                        <input type="file" name="file" multiple value='' encType="multipart/form-data" onChange={async (e) => await fetch('/api/projects/upload', {
-                            method: "POST",
-                            headers: {
-                                'Content-Type': "multipart/form-data"
-                            },
-                            body: e.target.files[0].webkitRelativePath
-                        }).then((data) => console.log(data.json()))} />
-                        {/* <button onSubmit={handleImageUpload} type="submit" name="upload" value="Upload" class="btn btn-success">Upload</button> */}
+                        {errors.map((error, idx) =>
+                            error === "Description must be between 20 and 50 characters" ? <li key={idx} id='error-list'>{error}</li> : null
+                        )}
                     </div>
-                    <button type="submit" name="upload" value="Upload" class="btn btn-success">Submit</button>
-                </form>
-            </div>
-        </>
+                <div>
+                    <input type="file" name="file" multiple value='' encType="multipart/form-data" onChange={async (e) => await fetch('/api/projects/upload', {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': "multipart/form-data"
+                        },
+                        body: e.target.files[0].webkitRelativePath
+                    }).then((data) => console.log(data.json()))} />
+                    {/* <button onSubmit={handleImageUpload} type="submit" name="upload" value="Upload" class="btn btn-success">Upload</button> */}
+                </div>
+                <button type="submit" name="upload" value="Upload" className="submit-button">Submit</button>
+            </form>
+        </div>
     )
 
 }
