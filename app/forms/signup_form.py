1edfe8a9b789a7f707e40184af16838ff185c8f7
Christo Grabowski <christo.grab@gmail.com>
2022-11-15 22:19:36 -0600

    added email validation to signup

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, ValidationError, Email
 from app.models import User
 
 
@@ -10,6 +10,12 @@ def user_exists(form, field):
     user = User.query.filter(User.email == email).first()
     if user:
         raise ValidationError('This email address is already in use.')
+        
+def is_email(form, field):
+    email = field.data
+    
+    if "@" not in email:
+        raise ValidationError("Please provide a valid email address")
 
 
 def username_exists(form, field):
@@ -21,8 +27,8 @@ def username_exists(form, field):
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired("Please enter a valid username"), username_exists])
-    email = StringField('email', validators=[DataRequired("Please provide a valid email address"), user_exists])
+        'username', validators=[DataRequired("Please provide a username"), username_exists])
+    email = StringField('email', validators=[DataRequired("Please provide an email address"), user_exists, is_email])
     password = StringField('password', validators=[DataRequired("Please provide a password")])
     first_name = StringField('first_name', validators=[DataRequired("Please provide your first name")])
     last_name = StringField("last_name", validators=[DataRequired("Please provide your last name")])
