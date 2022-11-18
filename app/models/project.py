1096fae799404d575484bd44f6ac7fcf235dd70d
Ali Ezzeddine <105993056+alkezz@users.noreply.github.com>
2022-11-18 16:06:41 -0500

    Update project.py

--- app/models/project.py
+++ app/models/project.py
@@ -20,7 +20,7 @@ class Project(db.Model):
   name = db.Column(db.String, nullable=False)
   description = db.Column(db.String)
   user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
-  images = db.Column(db.String(255))
+  images = db.Column(db.String)
   # Relationships
   user = db.relationship("User", back_populates="projects")
 #   images = db.relationship("ProjectImage", back_populates="project")
