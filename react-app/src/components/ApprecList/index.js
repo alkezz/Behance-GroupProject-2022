f99c54d3f298ea6298941aff4de59a88508ab24d
Jon Park <jonpark13@gmail.com>
2022-11-18 02:44:03 -0500

    stuff

---
+++ react-app/src/components/ApprecList/index.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+import {
+    useLocation,
+    useHistory,
+    Link
+} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux'
+import './ApprecList.css'
+// import * as profileActions from '../../store/songs'
+
+function AppeciationsList({appreciations}) {
+    const dispatch = useDispatch()
+    const location = useLocation()
+    const history = useHistory()
+    const [apprecList, setApprecList] = useState({})
+    const sessionUser = useSelector((state) => state.session);
+
+    const toObjFunc = (arr) => {
+        let newObj = {}
+        arr.forEach((e) => {
+            newObj[String(e)] = String(e)
+        })
+        return newObj
+    }
+
+    useEffect(() => {
+        if(appreciations.project_ids.length > 0){
+            fetch(`/api/projects/apprecRoute`, {
+                method: "POST",
+                headers: {"Content-Type":"application/json"},
+                body: JSON.stringify(toObjFunc(appreciations.project_ids))
+            }).then(res => res.json()).then(data => setApprecList(data))
+        }
+    }, [])
+
+    return (
+        <div className='userApprecGrid'> {
+            Object.values(apprecList).map(project => (
+                <div className='apprPreview' key={project.id}>
+                    <Link className='apprPreviewImgCont' to={{ pathname: `/gallery/${project.id}`, state: { background: location } }}><img className='apprPreviewImg' src={project.images[0]} /></Link>
+                    <Link className='apprUserText' to={`/${project.User.username}`}>
+                    {project.User.first_name} {project.User.last_name}
+                    </Link>
+                    <Link className='apprProjectText' to={`/gallery/${project.id}`}>
+                    {project.name}
+                    </Link>
+                    <div className='apprAppr'>
+                    <i className="apprIcon fa-solid fa-thumbs-up" />
+                    <div className='apprAppr_text'>{project.appreciations}</div>
+                    </div>
+                </div>
+            ))
+        }
+        </div>
+    )
+}
+export default AppeciationsList;
--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Redirect, Link, useLocation, useHistory } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, useParams, Redirect, Link, useLocation, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux'
+import AppeciationsList from '../ApprecList';
 import * as followActions from '../../store/follows'
 import avatar from '../../assets/behance-profile-image.png'
 import "./Profile.css"
+import MiniNav from '../MiniNav';
+import Rando from './rando';
 // import * as profileActions from '../../store/songs'
 
 function ProfilePage() {
@@ -16,7 +19,7 @@ function ProfilePage() {
   // console.log(prof)
   const [update, setUpdate] = useState(true)
   const [followerInfo, setFollowerInfo] = useState({})
-  const [apprecInfo, setApprecInfo] = useState({})
+  const [apprecInfo, setApprecInfo] = useState({"project_ids":[]})
   const { username } = useParams();
 
   const projList = prof.projects.map((project) => {
@@ -26,9 +29,9 @@ function ProfilePage() {
         <div className='userText'>
           {prof.first_name} {prof.last_name}
         </div>
-        <div className='projectText'>
+        <Link className='projectText' to={`/gallery/${project.id}`}>
           {project.name}
-        </div>
+        </Link>
         <div className='projectAppr'>
           <i className="apprIcon fa-solid fa-thumbs-up" />
           <div className='projectAppr_text'>{project.appreciations}</div>
@@ -46,7 +49,7 @@ function ProfilePage() {
     } else {
       followButton = (
         <button onClick={(e) => { handleFollow(e); setUpdate(!update) }} className='userCard_followBut' hidden={sessionUser.user.username.toLowerCase() === username.toLowerCase()}>
-          Follow
+          <i className="followIcon fa-solid fa-circle-plus" /> Follow
         </button>
       )
     }
@@ -148,7 +151,7 @@ function ProfilePage() {
               <div className='userStat'>
                 Followers
               </div>
-              <div className='userStat'>
+              <div className='userStatNum'>
                 {!!Object.keys(followerInfo).length && followerInfo.followed_by_user_ids.length}
               </div>
             </div>
@@ -156,16 +159,33 @@ function ProfilePage() {
               <div className='userStat'>
                 Following
               </div>
-              <div className='userStat'>
+              <div className='userStatNum'>
                 {!!Object.keys(followerInfo).length && followerInfo.current_followed_user_ids.length}
               </div>
             </div>
           </div>
+          <div>
+            You can email me here at:
+          </div>
+          <div>
+            {prof.email}
+          </div>
+          <div className='ourInfo'>
+            <Rando />
+          </div>
         </div>
         <div className='userProjects'>
-          <div className='userProjectsGrid' >
-            {!!prof && projList}
-          </div>
+        <MiniNav data={prof}></MiniNav>
+        <Switch>
+          <Route exact path={`/${prof.username}` || `/${prof.username}/work`}>
+            <div className='userProjectsGrid' >
+              {!!prof && projList}
+            </div>
+          </Route>
+          <Route path={`/${prof.username}/appreciations`}>
+            <AppeciationsList appreciations={apprecInfo}/>
+          </Route>
+        </Switch>
           {/* {!!prof && projList.length}
           {!!prof && JSON.stringify(prof)}
           {JSON.stringify(apprecInfo)}
