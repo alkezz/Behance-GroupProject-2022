4d8a920d6dd4c179250a6f4bcb9a4236d5394634
Christo Grabowski <christo.grab@gmail.com>
2022-11-18 21:41:03 -0600

    edit form bugfixing

--- react-app/src/components/EditProjectForm/index.js
+++ react-app/src/components/EditProjectForm/index.js
@@ -8,16 +8,16 @@ function EditProject() {
     const history = useHistory()
     const dispatch = useDispatch()
     const sessionUser = useSelector((state) => state.session.user);
-    const [name, setName] = useState("")
-    const [description, setDescription] = useState("")
-    const [images, setImages] = useState("")
-    const [url, setUrl] = useState([])
-    const [is_preview, setIsPreview] = useState(true)
-    const [errors, setErrors] = useState([])
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [images, setImages] = useState("");
+    const [url, setUrl] = useState([]);
+    const [is_preview, setIsPreview] = useState(true);
+    const [errors, setErrors] = useState([]);
     const [proj, setProj] = useState({});
-    const [submitted, setSubmitted] = useState(false)
+    const [submitted, setSubmitted] = useState(false);
     const formData = new FormData();
-    const { projectId } = useParams()
+    const { projectId } = useParams();
 
 
     if (!sessionUser) {
@@ -34,26 +34,29 @@ function EditProject() {
         if (name.length > 50 || name.length < 10) errorList.push("Name but be between 10 and 50 characters")
         if (description.length > 100 || description.length < 20) errorList.push("Description must be between 20 and 50 characters")
         setErrors(errorList)
-        if (errors.length) return
-        let imageInput = document.querySelector("#imageinput")
-        for (let i = 0; i < imageInput.files.length; i++) {
-            let img = imageInput.files[i]
-            console.log("IMG", img)
-            formData.append('file', img)
+        if (errorList.length) {
+            return
+        } else {
+            let imageInput = document.querySelector("#imageinput")
+            for (let i = 0; i < imageInput.files.length; i++) {
+                let img = imageInput.files[i]
+                console.log("IMG", img)
+                formData.append('file', img)
+            }
+            const pictures = await fetch(`/api/projects/upload`, {
+                method: "POST",
+                body: formData
+            }).then((res) => res.json())
+            const new_project = {
+                name,
+                description,
+                images: pictures.images
+            }
+            // console.log(new_project)
+            dispatch(projectActions.editProject(new_project, projectId)).then((data) => {
+                history.push(`/gallery/${data.id}`)
+            })
         }
-        const pictures = await fetch(`/api/projects/upload`, {
-            method: "POST",
-            body: formData
-        }).then((res) => res.json())
-        const new_project = {
-            name,
-            description,
-            images: pictures.images
-        }
-        // console.log(new_project)
-        dispatch(projectActions.editProject(new_project, projectId)).then((data) => {
-            history.push(`/gallery/${data.id}`)
-        })
     }
     return (
         <div className="create-project-container">
@@ -71,16 +74,16 @@ function EditProject() {
                     )}
                 </div>
                 <label>Provide a brief description of your project</label>
-                    <input
-                        type='text'
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        className="create-project-fields" />
-                    <div>
-                        {errors.map((error, idx) =>
-                            error === "Description must be between 20 and 50 characters" ? <li key={idx} id='error-list'>{error}</li> : null
-                        )}
-                    </div>
+                <input
+                    type='text'
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="create-project-fields" />
+                <div>
+                    {errors.map((error, idx) =>
+                        error === "Description must be between 20 and 50 characters" ? <li key={idx} id='error-list'>{error}</li> : null
+                    )}
+                </div>
                 <div className="create-project-image-container">
                     <div className="create-project-image-prompt">
                         Attach image files
